464886a7f0aca00858bd55b529914c113669d461
Bruno Rosa <bruno-rosa>
2024-12-02 19:55:14 -0300

    bootstrapped product module

--- online-store-backend/src/app.module.ts
+++ online-store-backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
 
 
 @Module({
   imports: [
     UsersModule,
-   MongooseModule.forRoot('mongodb://mongo:27017/online-store'), 
+   MongooseModule.forRoot('mongodb://mongo:27017/online-store'),
+   ProductsModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
